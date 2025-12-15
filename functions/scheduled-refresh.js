013700f016b1dbc62a6861351236de96b533cdae
AppexDigital <appexarquitectodigital@gmail.com>
2025-12-15 09:03:21 -0600

    Update scheduled-refresh.js

--- functions/scheduled-refresh.js
+++ functions/scheduled-refresh.js
@@ -123,4 +123,4 @@ const myHandler = async (event, context) => {
 //module.exports.handler = schedule("0 6,18 * * *", myHandler);
 
 // CAMBIO TEMPORAL PARA PRUEBA (8:36 AM CR / 14:36 UTC)
-module.exports.handler = schedule("36 14 * * *", myHandler);
+module.exports.handler = schedule("5 15 * * *", myHandler);
